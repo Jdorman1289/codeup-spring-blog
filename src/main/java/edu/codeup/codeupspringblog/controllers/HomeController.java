4f9b5389fa5c504ef3c58e42f100a4b260ca55c4
Jesse <Jdorman1289@gmail.com>
2023-09-18 14:42:58 -0500

    fix problems caused from merge

--- src/main/java/edu/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,15 +1,15 @@
-package edu.codeup.codeupspringblog.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage() {
-        return "This is the landing page!";
-    }
-}
+//package edu.codeup.codeupspringblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String landingPage() {
+//        return "This is the landing page!";
+//    }
+//}
